1abcc48355976088b8a7a440b548ecf09585d070
ManuMnt <manu.mts@icloud.com>
2023-06-10 11:17:40 +0200

    Added oldest prices

--- AsDOr/games/models.py
+++ AsDOr/games/models.py
@@ -24,12 +24,40 @@ class Game(models.Model):
 
 class Award(models.Model):
     CATEGORIES = [
-        ("Jeu de l'année",          "GAME_OF_THE_YEAR"),
-        ("Jeu de l'année 'Enfant'", "KID_GAME_OF_THE_YEAR"),
-        ("Jeu de l'année 'Expert'", "EXPERT_GAME_OF_THE_YEAR"),
-        ("Jeu de l'année 'Initié'", "NOOB_GAME_OF_THE_YEAR"),
-        ("Prix du Jury",            "JURY_PRIZE"),
-        ("Grand Prix",              "BIG_PRIZE")
+        ("Jeu de l'année",                              "GAME_OF_THE_YEAR"),
+        ("Jeu de l'année 'Enfant'",                     "KID_GAME_OF_THE_YEAR"),
+        ("Jeu de l'année 'Expert'",                     "EXPERT_GAME_OF_THE_YEAR"),
+        ("Jeu de l'année 'Initié'",                     "NOOB_GAME_OF_THE_YEAR"),
+        ("Prix du Jury",                                "JURY_PRIZE"),
+        ("Grand Prix",                                  "BIG_PRIZE"),
+        ("Mention Spéciale",                            "MENTION_SPECIALE"),
+        ("Super As D'Or",                               "AS_OR_SUPER"),
+        ("As D'Or",                                     "AS_OR"),
+        ("As D'Or Jeu d'Imagination",                   "AS_OR_IMAGINATION"),
+        ("As D'Or Jeu Vidéo",                           "AS_OR_JEU_VIDEO"),
+        ("As D'Or Jeu de Convivialité",                 "AS_OR_CONVIVIALITE"),
+        ("As D'Or Jeu de Stratégie",                    "AS_OR_STRATEGIE"),
+        ("As D'Or Jeu d'Aventure",                      "AS_OR_AVENTUREA"),
+        ("As D'Or Jeu de Simulation",                   "AS_OR_SIMULATION"),
+        ("As D'Or Jeu d'Informatique",                  "AS_OR_INFORMATIQUE"),
+        ("As D'Or Jeu de Lettres",                      "AS_OR_LETTRES"),
+        ("As D'Or Jeu de Lettres et de Connaissances",  "AS_OR_LETTRES_CONNAISSANCES"),
+        ("As D'Or Jeu Espoir",                          "AS_OR_ESPOIR"),
+        ("As D'Or Jeu Junior (13 - 17 ans)",            "AS_OR_JUNIOR"),
+        ("As D'Or Jeu Benjamin (4 - 7 ans)",            "AS_OR_BENJAMIN"),
+        ("As D'Or Jeu Cadet (8 - 12 ans)",              "AS_OR_CADET"),
+        ("As D'Or Jeu de Société",                      "AS_OR_SOCIETE"),
+        ("As D'Or Jeu de Société d'Intéractif",         "AS_OR_SOCIETE_INTERACTIF"),
+        ("As D'Or Jeu Joker",                           "AS_OR_JOKER"),
+        ("As D'Or Prix Spécial du Jury",                "AS_OR_SPECIAL_JURY"),
+        ("As D'Or Jeu de Dés",                          "AS_OR_DES"),
+        ("As D'Or Jeu Innovation",                      "AS_OR_INNOVATION"),
+        ("As D'Or Jeu de Tactique",                     "AS_OR_TACTIQUE"),
+        ("As D'Or Jeu de Vocabulaire",                  "AS_OR_VOCABULAIRE"),
+        ("As D'Or Jeu de Cartes à collectionner",       "AS_OR_CARTES_COLLECTION"),
+        ("As D'Or Jeu Familial",                        "AS_OR_FAMILIALAS_OR_FAMILIAL"),
+        ("As D'Or Jeu de la Décennie",                  "AS_OR_DECENNIE"),
+        ("As D'Or Jeu de Réfléxion",                    "AS_OR_REFLEXION")
     ]
     year = models.CharField(max_length=4)
     category = models.CharField(choices=CATEGORIES, max_length=50)
@@ -66,38 +94,3 @@ class Award(models.Model):
 class Wishlist(models.Model):
     user = models.ForeignKey(User, on_delete=models.CASCADE)
     games = models.ManyToManyField(Game, related_name="games")
-
-
-
-"""
-  ,
-    # Old prices
-    ("MENTION_SPECIALE           ", "Mention Spéciale"),
-    ("AS_OR_SUPER                ", "Super As D'Or"),
-    ("AS_OR                      ", "As D'Or"),
-    ("AS_OR_IMAGINATION          ", "As D'Or Jeu d'Imagination"),
-    ("AS_OR_JEU_VIDEO            ", "As D'Or Jeu Vidéo"),
-    ("AS_OR_CONVIVIALITE         ", "As D'Or Jeu de Convivialité"),
-    ("AS_OR_STRATEGIE            ", "As D'Or Jeu de Stratégie"),
-    ("AS_OR_AVENTURE             ", "As D'Or Jeu d'Aventure"),
-    ("AS_OR_SIMULATION           ", "As D'Or Jeu de Simulation"),
-    ("AS_OR_INFORMATIQUE         ", "As D'Or Jeu d'Informatique"),
-    ("AS_OR_LETTRES              ", "As D'Or Jeu de Lettres"),
-    ("AS_OR_LETTRES_CONNAISSANCES", "As D'Or Jeu de Lettres et de Connaissances"),
-    ("AS_OR_ESPOIR               ", "As D'Or Jeu Espoir"),
-    ("AS_OR_JUNIOR               ", "As D'Or Jeu Junior (13 - 17 ans)"),
-    ("AS_OR_BENJAMIN             ", "As D'Or Jeu Benjamin (4 - 7 ans)"),
-    ("AS_OR_CADET                ", "As D'Or Jeu Cadet (8 - 12 ans)"),
-    ("AS_OR_SOCIETE              ", "As D'Or Jeu de Société"),
-    ("AS_OR_SOCIETE_INTERACTIF   ", "As D'Or Jeu de Société d'Intéractif"),
-    ("AS_OR_JOKER                ", "As D'Or Jeu Joker"),
-    ("AS_OR_SPECIAL_JURY         ", "As D'Or Prix Spécial du Jury"),
-    ("AS_OR_DES                  ", "As D'Or Jeu de Dés"),
-    ("AS_OR_INNOVATION           ", "As D'Or Jeu Innovation"),
-    ("AS_OR_TACTIQUE             ", "As D'Or Jeu de Tactique"),
-    ("AS_OR_VOCABULAIRE          ", "As D'Or Jeu de Vocabulaire"),
-    ("AS_OR_CARTES_COLLECTION    ", "As D'Or Jeu de Cartes à collectionner"),
-    ("AS_OR_FAMILIAL             ", "As D'Or Jeu Familial"),
-    ("AS_OR_DECENNIE             ", "As D'Or Jeu de la Décennie"),
-    ("AS_OR_REFLEXION            ", "As D'Or Jeu de Réfléxion")
-  """
